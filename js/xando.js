5d2cfe226934dd3a0cdf765fb1d0a7ccffae9626
Your Name <you@example.com>
2024-04-23 18:01:53 +0200

    first commit

---
+++ js/xando.js
@@ -0,0 +1,58 @@
+
+
+var name1 = document.getElementById('nameJ');
+var age = document.getElementById('numJ');
+var skills = document.getElementById('masJ');
+// mainList = [];
+var mainList;
+if (localStorage.getItem('mainList') !== null) {
+    mainList = JSON.parse(localStorage.getItem('mainList'));
+    displaylist();
+  
+}
+else {
+    mainList = [];
+  
+}
+var  tableBady = document.getElementById("tableDisplay"); 
+
+function clickJ(){
+  
+    var listJs = {
+        name: name1.value,
+        age: Number(age.value),
+        skills: skills.value
+    };
+    // لتخزين الاراى
+    mainList.push(listJs)
+    localStorage.setItem('mainList',JSON.stringify(mainList));
+    // لعرض الارارى 
+    // console.log(mainList)
+    displaylist()
+} 
+
+function displaylist() {
+    var tableJS =``;
+    for (let index = 0; index < mainList.length; index++) {
+        tableJS += `
+        <tr>
+        <td scope="row"> ${index}</td>
+        <td>${mainList[index].name}</td>
+        <td>${mainList[index].age}</td>
+        <td>${mainList[index].skills}</td>
+        <td><button></button>delete</td>
+        <td><button></button>edite</td>
+      </tr>
+        `
+
+        
+    }
+    tableBady.innerHTML = tableJS;
+    
+}
+  
+
+
+
+
+
