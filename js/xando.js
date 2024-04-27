60429224039416374c26d642913adbd6fa0da4ef
Your Name <you@example.com>
2024-04-27 21:49:53 +0300

    button search

--- js/xando.js
+++ js/xando.js
@@ -1,9 +1,10 @@
 var name1 = document.getElementById("nameJ");
 var age = document.getElementById("numJ");
 var skills = document.getElementById("masJ");
+var mainList;
 //  var mainList = [];
 var tableBady = document.getElementById("tableDisplay");
-var mainList;
+
 if (localStorage.getItem("mainList") !== null) {
   mainList = JSON.parse(localStorage.getItem("mainList"));
   displaylist();
@@ -25,48 +26,58 @@ function clickJ() {
   displaylist();
 }
 
-function displaylist() {
-  //   var tableJS = ``;
-  //   for (var iteam of mainList) {
-  //     tableJS += `
-  //         <tr>
-  //         <td scope="row">1</td>
-  //         <td>${iteam.name}</td>
-  //         <td>${iteam.age}</td>
-  //         <td>${iteam.skills}</td>
-  //         <td><button></button>delete</td>
-  //         <td><button></button>edite</td>
-  //       </tr>
-  //         `;
-  //   }
+function displaylist(list = mainList) {
+  var tableJS = ``;
+  var forIndex = 0;
+  for (var iteam of list) {
+    tableJS += `
+          <tr>
+          <td scope="row">1</td>
+          <td>${iteam.name}</td>
+          <td>${iteam.age}</td>
+          <td>${iteam.skills}</td>
+         <td><button onclick="delBtn(${forIndex})" class="btn btn-danger">delete</button></td>
+        <td><button class="btn btn-warning">edite</button></td>
+         </tr>
+          `;
+    forIndex++;
+  }
 
-  // tableBady.innerHTML = tableJS;
+  tableBady.innerHTML = tableJS;
 
   // -------------------------------------
 
-  var tableJS = ``;
-  for (let index = 0; index < mainList.length; index++) {
-    tableJS += `
-        <tr>
-        <td scope="row"> ${index}</td>
-        <td>${mainList[index].name}</td>
-        <td>${mainList[index].age}</td>
-        <td>${mainList[index].skills}</td>
-        <td><button onclick="delBtn(${index})" class="btn btn-danger">delete</button></td>
-        <td><button class="btn btn-warning">edite</button></td>
-      </tr>
-        `;
-  }
-  tableBady.innerHTML = tableJS;
+  // var tableJS = ``;
+  // for (let index = 0; index < mainList.length; index++) {
+  //   tableJS += `
+  //       <tr>
+  //       <td scope="row"> ${index}</td>
+  //       <td>${mainList[index].name}</td>
+  //       <td>${mainList[index].age}</td>
+  //       <td>${mainList[index].skills}</td>
+  //        <td><button onclick="delBtn(${index})" class="btn btn-danger">delete</button></td>
+  //       <td><button class="btn btn-warning">edite</button></td>
+  //     </tr>
+  //       `;
+  // }
+  // tableBady.innerHTML = tableJS;
 }
 
 function delBtn(index) {
   console.log("hello");
-  mainList.splice(index, 1)
-localStorage.setItem("mainList", JSON.stringify(mainList));
+  mainList.splice(index, 1);
+  localStorage.setItem("mainList", JSON.stringify(mainList));
   displaylist();
 }
 
 function searchBtn(keysearch) {
-  console.log(keysearch);
+  var forsearchBtn = [];
+  for (let index = 0; index < mainList.length; index++) {
+    if (mainList[index].name.includes(keysearch)) {
+      forsearchBtn.push(mainList[index]);
+      displaylist(forsearchBtn);
+
+      console.log(forsearchBtn);
+    }
+  }
 }
